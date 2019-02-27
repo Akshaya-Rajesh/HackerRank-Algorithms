9e793b515ccb2746c7a1c8bd37f9ea23d9d5695b
Akshaya Rajesh <akshayatr1997@gmail.com>
2019-02-27 10:42:28 +0530

    Update A Very Big Sum.py

--- A Very Big Sum.py
+++ A Very Big Sum.py
@@ -1,14 +1,14 @@
 #!/bin/python3
 
-import sys
-
 def aVeryBigSum(n,ar):
+    #initializing sum as the 1st element of the list
     sum=ar[0] 
+    #adding each of the element to the variable sum
     for i in range(1,n):
         sum+=ar[i]
     return(sum)
 
-n = int(input().strip())
-ar = list(map(int, input().strip().split(' ')))
+n = int(input())
+ar = list(map(int, input().split(' ')))
 result = aVeryBigSum(n,ar)
 print(result)
