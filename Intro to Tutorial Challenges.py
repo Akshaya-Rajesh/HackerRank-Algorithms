c72efd654e110b7de2c0c1f0f4a5ded0ab673a9b
Akshaya Rajesh <akshayatr1997@gmail.com>
2019-03-09 21:18:38 +0530

    Create Intro to Tutorial Challenges.py

---
+++ Intro to Tutorial Challenges.py
@@ -0,0 +1,16 @@
+#!/binimport sys
+
+def introTutorial(V, arr):
+    for i in range(0,n):
+        if(arr[i]==V):
+            return(i)
+    return(-1)
+
+if __name__ == "__main__":
+    V = int(input().strip())
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = introTutorial(V, arr)
+    print(result)
+/python3
+
