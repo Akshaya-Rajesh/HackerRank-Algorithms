fc3a3b8b0e3feec8d1d03598dcf2e76930030524
Akshaya Rajesh <akshayatr1997@gmail.com>
2019-07-29 23:36:20 +0530

    Create Lisa's Workbook.py

---
+++ Lisa's Workbook.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the workbook function below.
+def workbook(n, k, arr):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
